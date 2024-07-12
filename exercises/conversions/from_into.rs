285d3245c3c361d389a5c6a72cee0aa29ee7df0d
singlemice <singlemice@gmail.com>
2024-07-12 13:13:12 +0800

    update

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -40,10 +40,34 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of
 // Person Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
 
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        if s.len() == 0 {
+            return Person::default()
+        }
+        else{
+            s.split( ",").map(|x| x.into()).collect::<Vec<String>>().into()
+        }
+    }
+}
+
+impl From<Vec<String>> for Person  {
+    fn from(s: Vec<String>) -> Person {
+        if s.len() != 2 {
+            return  Person::default();
+        }
+        let n = s[1].parse::<usize>();
+        let age;
+        if n.is_err()|| s[0]==""{
+            return Person::default();
+        }else {
+            age = n.unwrap();
+            Person {
+                name: s[0].to_string(),
+                age: age,
+            }
+        }
     }
 }
 
